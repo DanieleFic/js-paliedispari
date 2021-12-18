43f1b0a3fa502997c5bed215e0b1e9887f7ade60
DanieleFic <reb_ficus22@hotmail.it>
2021-12-18 16:54:39 +0100

    esercizio palindromi

--- js-palindromi/myscript.js
+++ js-palindromi/myscript.js
@@ -1,1 +1,34 @@
-console.log("aassdd")
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+
+// function that check str is palindrome or not
+function controlloParolaPalindroma() {
+    let lunghezza = myArrayParola.length;
+    for (let  i = 0; i < lunghezza  ; i++) {
+        if (myArrayParola[i] == myArrayParola[lunghezza - 1 - i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+let parola = prompt('inserisci una parola');
+let myArrayParola = [];
+
+for(i = 0 ; i < parola.length; i++){
+    myArrayParola.push(parola[i]);
+    console.log(parola[i]);
+}
+
+
+
+
+
+if (controlloParolaPalindroma(parola)) {
+    document.write("La Parola è Palindroma");
+} else {
+    document.write("La Parola non è Palindroma");
+}
